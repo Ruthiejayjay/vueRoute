a8b2e1c7a61df1895002af4e24a394b74960a257
ruth <jamesruth449@gmail.com>
2021-09-26 22:53:13 -0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed ratione
+      repellendus aperiam quis quod eos illum cum aliquam impedit, nam error
+      possimus labore aspernatur asperiores voluptates eum. Earum perferendis
+      sint quo veniam magnam. Voluptates molestiae consequatur ipsum, totam
+      provident aspernatur explicabo. Neque dolorem aliquid odio perspiciatis
+      quos eligendi quibusdam, expedita quas debitis ut doloremque quaerat
+      quidem id in reprehenderit consequatur ipsum error nulla ad mollitia
+      voluptas at. Repudiandae, qui rerum! Voluptates perferendis nulla delectus
+      dolore cumque eos sapiente officiis odio aspernatur consequatur deleniti
+      cupiditate optio, accusantium, minus qui dolorem voluptate pariatur
+      quaerat? Cum id culpa non, deleniti voluptas nisi explicabo?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed ratione
+      repellendus aperiam quis quod eos illum cum aliquam impedit, nam error
+      possimus labore aspernatur asperiores voluptates eum. Earum perferendis
+      sint quo veniam magnam. Voluptates molestiae consequatur ipsum, totam
+      provident aspernatur explicabo. Neque dolorem aliquid odio perspiciatis
+      quos eligendi quibusdam, expedita quas debitis ut doloremque quaerat
+      quidem id in reprehenderit consequatur ipsum error nulla ad mollitia
+      voluptas at. Repudiandae, qui rerum! Voluptates perferendis nulla delectus
+      dolore cumque eos sapiente officiis odio aspernatur consequatur deleniti
+      cupiditate optio, accusantium, minus qui dolorem voluptate pariatur
+      quaerat? Cum id culpa non, deleniti voluptas nisi explicabo?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
